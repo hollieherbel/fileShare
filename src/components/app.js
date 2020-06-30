78bae7a17da07359afca5ec8061a3641eba58509
hollieherbel <herbel6521@gmail.com>
2020-06-30 15:06:50 -0500

    added handlers to send and receive files

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,9 @@ export default class App extends Component {
     this.handleChange = this.handleChange.bind(this)
     this.handleDownload = this.handleDownload.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleGetData = this.handleGetData.bind(this)
   }
+
 
   handleChange(event) {
     this.setState({ file: event.target.files[0] })
@@ -42,11 +44,23 @@ export default class App extends Component {
       .catch(error => console.log(error))
   }
   
+  handleGetData() {
+    fetch("http://127.0.0.1:5000/file/get/1", { method: "GET"})
+    .then(response => response.blob())
+    .then(data => {
+      const file = new File([data], "Test.jpg", { type: "image/jpeg" })
+      this.setState({  file: file })
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className='app'>
         <input onChange={this.handleChange} type="file"></input>
         <button onClick={this.handleSubmit}>Send</button>
+        <hr />
+        <button onClick={this.handleGetData}>Get File</button>
         <button onClick={this.handleDownload}>Download</button>
       </div>
     );
